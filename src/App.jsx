4b514cb00753be13e49ed586378f42d02e26bc2d
Carlosgv95 <cagv.1995@outlook.com>
2025-03-16 15:08:35 -0300

    add Register & Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Footer from "./components/Footer/Footer"
 import Home from "./components/Home/Home"
 import Navbar from "./components/Navbar/Navbar"
+import Register from "./components/Register/Registro"
+import Login from "./components/Login/Login"
 
 const App = () => {
 
   return (
     <>
       <Navbar />
-      <Home /> 
+      {/* <Register/> */}
+      <Login />
+      {/* <Home />   */}
       <Footer />
     </>
   )
